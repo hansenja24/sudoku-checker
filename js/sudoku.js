c4a558e8d33fb409b632be9b96d981a3644ad93d
Hansen Hansen and Patrick Napper <patrick@>
2017-11-14 13:35:45 -0800

    add functions and test specs for multiple squares and CheckAll for entire board. Tests specs are passing.

--- js/sudoku.js
+++ js/sudoku.js
@@ -53,29 +53,120 @@ Sudoku.prototype.AllRowCheck = function(row1, row2, row3, row4, row5, row6, row7
   return true;
 }
 
-Sudoku.prototype.CheckBoxes = function(row1, row2, row3){
-    var rowList = [row1, row2, row3];
+Sudoku.prototype.CheckBoxes = function(row1, row2, row3, row4, row5, row6, row7, row8, row9){
+    var rowList = [row1, row2, row3, row4, row5, row6, row7, row8, row9];
     box = [];
 
-    for(i = 0; i < 3; i++)
+    for(g = 0; g < 9; g+=3)
     {
-      for(j = 0; j < 3; j++)
+      for(h = 0; h < 9; h+=3)
       {
-        box.push(rowList[i][j]);
+        for(i = 0 + g; i < 3 + g; i++)
+        {
+          for(j = 0 + h; j < 3 + h; j++)
+          {
+            box.push(rowList[i][j]);
+          }
+        }
+
+        for (i = 0; i < 9; i++)
+        {
+          for(j = i+1; j < 9; j++)
+          {
+            if(box[i] == box[j])
+            {
+              return false;
+            }
+          }
+
+        }
+        box = [];
       }
+
     }
-    
+    return true;
+}
+
+Sudoku.prototype.CheckAll = function(row1, row2, row3, row4, row5, row6, row7, row8, row9){
+  var rowList = [row1, row2, row3, row4, row5, row6, row7, row8, row9];
+  box = [];
+///Row check
+
+  for(g = 0; g < 9; g++)
+  {
     for (i = 0; i < 9; i++)
     {
       for(j = i+1; j < 9; j++)
       {
-        if(box[i] == box[j])
+        if(rowList[g][i] == rowList[g][j])
         {
           return false;
         }
       }
     }
-    return true;
+  }
+
+//Col check
+
+for(g = 0; g < 9; g++)
+{
+  for (i = 0; i < 9; i++)
+  {
+    for(j = i+1; j < 9; j++)
+    {
+      if(rowList[i][g] == rowList[j][g])
+      {
+        return false;
+      }
+    }
+  }
+}
+
+//All row check
+  for (i = 0; i < 9; i++)
+  {
+    for(j = i+1; j < 9; j++)
+    {
+      for(k = 0; k < 9; k++)
+      {
+        if(rowList[i][k] == rowList[j][k])
+        {
+          return false;
+        }
+      }
+    }
+  }
+
+///All Boxes Check
+  for(g = 0; g < 9; g+=3)
+  {
+    for(h = 0; h < 9; h+=3)
+    {
+      for(i = 0 + g; i < 3 + g; i++)
+      {
+        for(j = 0 + h; j < 3 + h; j++)
+        {
+          box.push(rowList[i][j]);
+        }
+      }
+
+      for (i = 0; i < 9; i++)
+      {
+        for(j = i+1; j < 9; j++)
+        {
+          if(box[i] == box[j])
+          {
+            return false;
+          }
+        }
+
+      }
+      box = [];
+    }
+
+  }
+  return true;
+
 }
 
 exports.sudokuModule = Sudoku;
--- spec/sudoku-spec.js
+++ spec/sudoku-spec.js
@@ -17,8 +17,13 @@ describe('Sudoku', function() {
       expect(sudokuCheck.AllRowCheck([1,2,3,4,5,6,7,8,9],[2,3,4,5,6,7,8,9,1],[3,4,5,6,7,8,9,1,2],[4,5,6,7,8,9,1,2,3],[5,6,7,8,9,1,2,3,4],[6,7,8,9,1,2,3,4,5],[7,8,9,1,2,3,4,5,6],[8,9,1,2,3,4,5,6,7],[9,1,2,3,4,5,6,7,8])).toEqual(true)
     });
 
-    it('should test whether a Sudoku first box is true', function() {
+    it('should test whether a Sudoku all Box is true', function() {
       var sudokuCheck = new Sudoku()
-      expect(sudokuCheck.CheckBoxes([1,2,3,4,5,6,7,8,9],[4,5,6,7,8,9,1,2,3],[7,8,9,1,2,3,4,5,6])).toEqual(true)
+      expect(sudokuCheck.CheckBoxes([9,5,4,8,7,2,3,1,6],[8,6,1,9,4,3,7,2,5],[3,2,7,6,5,1,4,9,8],[1,3,2,5,9,7,8,6,4],[7,4,9,2,8,6,5,3,1],[5,8,6,1,3,4,2,7,9],[2,9,8,7,6,5,1,4,3],[4,1,5,3,2,9,6,8,7],[6,7,3,4,1,8,9,5,2])).toEqual(true)
+    });
+
+    it('should test whether a Sudoku all Sudoku is true', function() {
+      var sudokuCheck = new Sudoku()
+      expect(sudokuCheck.CheckAll([9,5,4,8,7,2,3,1,6],[8,6,1,9,4,3,7,2,5],[3,2,7,6,5,1,4,9,8],[1,3,2,5,9,7,8,6,4],[7,4,9,2,8,6,5,3,1],[5,8,6,1,3,4,2,7,9],[2,9,8,7,6,5,1,4,3],[4,1,5,3,2,9,6,8,7],[6,7,3,4,1,8,9,5,2])).toEqual(true)
     });
 });
